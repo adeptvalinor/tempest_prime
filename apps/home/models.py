60e8d4b851dbeed467bb362d15b4653a1abc683c
adeptvalinor <sean@nhbx.onmicrosoft.com>
2024-03-04 17:46:54 +0000

    Update Template

--- apps/home/models.py
+++ apps/home/models.py
@@ -8,3 +8,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Server(models.Model):
+    ip = models.GenericIPAddressField()
+    status = models.CharField(max_length=50)
+    user = models.CharField(max_length=100)
+    key = models.TextField()
+    port = models.PositiveIntegerField()
+    operating_system = models.CharField(max_length=100)
+    remarks = models.TextField()
--- core/settings.py
+++ core/settings.py
@@ -18,8 +18,10 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS        = ['localhost', 'localhost:85', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS        = ['localhost', 'rey.jedi.my', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+CSRF_TRUSTED_ORIGINS = ['https://rey.jedi.my', 'http://127.0.0.1', 'https://' + config('SERVER', default='127.0.0.1')]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Application definition
 
